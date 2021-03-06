81bd73e7dc17ee10004448bdf4a789242a37d875
ShinyaS-cloud <spyken123@gmail.com>
2021-03-06 13:21:32 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import bodyParser from 'body-parser'
+
+interface Error {
+  status?: number
+  message?: string
+}
+const app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
